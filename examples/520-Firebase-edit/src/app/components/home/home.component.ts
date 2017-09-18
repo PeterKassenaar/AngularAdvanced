8dd7b9d0f62fd5ebbb10e4fac6d4fced1305e59c
PeterKassenaar <info@kassenaar.com>
2017-09-18 16:40:00 +0200

    Added example 520-firebase-edit

---
+++ examples/520-Firebase-edit/src/app/components/home/home.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {FirebaseListObservable} from 'angularfire2/database';
+import {City} from '../../shared/model/city.model';
+import {CityService} from '../../shared/services/city.service';
+
+@Component({
+	selector   : 'app-home',
+	templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+	cities$: FirebaseListObservable<City[]>;
+
+	constructor(private cityService:CityService) {
+
+	}
+
+	ngOnInit() {
+		this.cities$ = this.cityService.getCities();
+	}
+
+	remove(city){
+		this.cityService.removeCity(city);
+	}
+
+
+}
