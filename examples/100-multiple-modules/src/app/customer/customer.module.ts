a124372bd62987909958c2701fadea53cba1d74f
Peter Kassenaar <info@kassenaar.com>
2018-06-20 19:08:43 +0200

    Updated angular.json

--- examples/100-multiple-modules/src/app/customer/customer.module.ts
+++ examples/100-multiple-modules/src/app/customer/customer.module.ts
@@ -1,19 +1,15 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 // import shared module (with search field)
-import {SharedModule} from '../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 
 // components for this module
-import {CustomerComponent} from './customer.component';
+import { CustomerComponent } from './customer.component';
 
 @NgModule({
-	imports     : [
-		CommonModule,
-		SharedModule
-	],
-	exports     : [CustomerComponent], // component must be exported, otherwise it can't be used in other modules
-	declarations: [CustomerComponent]
+	imports: [CommonModule, SharedModule],
+	declarations: [CustomerComponent],
+	exports: [CustomerComponent] // component must be exported, otherwise it can't be used in other modules
 })
-export class CustomerModule {
-}
+export class CustomerModule {}
