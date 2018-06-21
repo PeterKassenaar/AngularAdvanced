b9fc74ca6a72534474093b24767696c29b7988f4
Peter Kassenaar <info@kassenaar.com>
2018-06-21 14:40:33 +0200

    Added routing events example

--- examples/120-custom-preloading/src/app/app.component.ts
+++ examples/120-custom-preloading/src/app/app.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
+import { filter, map } from 'rxjs/operators';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	constructor(private router: Router) {}
+
+	ngOnInit() {
+		// 1a. subscribing to router events
+		// this.router.events.subscribe(event => {
+		// 	console.log(event);
+		// });
+		// 1b. subscribing, using RxJS operators to manipulate the event
+		// this.router.events
+		// 	.pipe(
+		// 		filter(event => event instanceof NavigationStart),
+		// 		map(
+		// 			(e: NavigationStart) =>
+		// 				'started navigiation to url ::' + e.url
+		// 		)
+		// 	)
+		// 	.subscribe(res => console.log(res));
+	}
 }
