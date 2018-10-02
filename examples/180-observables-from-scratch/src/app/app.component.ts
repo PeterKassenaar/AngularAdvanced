184788965abc74c645b0da537f6c28cb1e8906c8
Peter Kassenaar <info@kassenaar.com>
2018-10-02 10:43:41 +0200

    Updated Observable Example

--- examples/180-observables-from-scratch/src/app/app.component.ts
+++ examples/180-observables-from-scratch/src/app/app.component.ts
@@ -1,26 +1,31 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
-
-interface Person {
-  name: string;
-  email: string;
-}
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {fromEvent, Observable, of, Subscription, throwError} from 'rxjs';
+import {catchError, filter, finalize, map, switchMap, take, takeUntil, takeWhile} from 'rxjs/operators';
+import {assertNumber} from '@angular/core/src/render3/assert';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
 
-  @ViewChild('btn') btn: ElementRef;
-  msg = 'Hello World';
-  counter = 0;
+    @ViewChild('btn') btn: ElementRef;
+    @ViewChild('txtInput') input: ElementRef;
+    msg$: Observable<any>;
+    counter: number = 0;
+    sub: Subscription;
 
-  ngOnInit() {
-    // Create an observable from the 'click' event
-    // fromEvent(this.btn.nativeElement, 'click')
-    //   .subscribe(res => this.msg = 'Goodbye World ' + ++this.counter);
-  }
-
+    ngOnInit() {
+        // Create an observable from the 'click' event
+        //1. Hier komt de data vandaan
+        this.msg$ = fromEvent(this.btn.nativeElement, 'click')
+            .pipe(
+                filter((event: MouseEvent) => event.shiftKey),
+                map(event => {
+                    this.counter++;
+                    return this.input.nativeElement.value;
+                }),
+                finalize(() => console.log('Afgelopen'))
+            );
+    }
 }
