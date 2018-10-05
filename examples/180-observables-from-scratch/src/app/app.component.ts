76f9c0aaf24b68b0e8e68e4b9bae4635addadb24
PeterKassenaar <info@kassenaar.com>
2018-10-05 09:01:21 +0200

    Update example on Observables

--- examples/180-observables-from-scratch/src/app/app.component.ts
+++ examples/180-observables-from-scratch/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {fromEvent, Observable, of, Subscription, throwError} from 'rxjs';
-import {catchError, filter, finalize, map, switchMap, take, takeUntil, takeWhile} from 'rxjs/operators';
-import {assertNumber} from '@angular/core/src/render3/assert';
+import {filter, finalize, map, switchMap, take, takeUntil, takeWhile} from 'rxjs/operators';
+
 
 @Component({
     selector: 'app-root',
@@ -11,23 +11,24 @@ export class AppComponent implements OnInit {
 
     @ViewChild('btn') btn: ElementRef;
     @ViewChild('txtInput') input: ElementRef;
-    msg$: Observable<any>;
-    counter: number = 0;
+    msg = 'Hello World';
+    counter = 0;
     sub: Subscription;
-    counter$ = of(this.counter > 3);
 
     ngOnInit() {
         // Create an observable from the 'click' event
-        //1. Hier komt de data vandaan
-        this.msg$ = fromEvent(this.btn.nativeElement, 'click')
+        //1. Data input - we create an observable from the click event of a button
+        fromEvent(this.btn.nativeElement, 'click')
+        // 2. using the .pipe() method to apply 0, 1 or more operators on the stream
             .pipe(
-                filter((event: MouseEvent) => event.shiftKey),  // filter shiftKey
-                takeWhile(()=> this.counter < 3),               // totdat counter >= 3
                 map(event => {                                  // counter ophogen, waarde emitten
                     this.counter++;
-                    return this.input.nativeElement.value;
-                }),
-                finalize(() => console.log('Afgelopen'))        // bij unsubscribe()
-            );
+                    return 'Goodbye world';
+                })
+                //... more operators, see for example https://www.learnrxjs.io/
+            )
+            // 3. Final input, in the form of a .subscribe() block.
+            //   We can also use the | async pipe in the view to let Angular handle subscriptions
+            .subscribe(result => this.msg = result);
     }
 }
