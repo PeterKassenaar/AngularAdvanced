efb5da502f92b7c02acb823754f61dc3056e461d
PeterKassenaar <info@kassenaar.com>
2019-06-20 15:07:01 +0200

    Update Lazy Loading syntax for Angular 8+

--- examples/110-lazy-loading/src/app/app.routing.module.ts
+++ examples/110-lazy-loading/src/app/app.routing.module.ts
@@ -3,16 +3,25 @@ import {ExtraOptions, PreloadAllModules, RouterModule, Routes} from '@angular/ro
 // Note: no import from the various components. Only references to
 // the modules, containing the components
 const routes: Routes = [
-	{path: '', redirectTo: 'customers', pathMatch: 'full'},
-	{path: 'customers', loadChildren: './customer/customer.module#CustomerModule'},
-	{path: 'products', loadChildren: './products/products.module#ProductsModule'},
-	// New notation (Angular 8+):
-	// {path: 'customer', loadChildren:()=> import('./customer/customer.module').then(mod => mod.CustomersModule)}
+    {path: '', redirectTo: 'customers', pathMatch: 'full'},
+    {path: 'customers', loadChildren: './customer/customer.module#CustomerModule'},
+    {path: 'products', loadChildren: './products/products.module#ProductsModule'},
+    // New notation (Angular 8+):
+    // {
+    //     path: 'customers',
+    //     loadChildren: () => import('./customer/customer.module')
+    //         .then(mod => mod.CustomerModule)
+    // },
+    // {
+    //     path: 'products',
+    //     loadChildren: () => import('./products/products.module')
+    //         .then(mod => mod.ProductsModule)
+    // },
 ];
 
-const config: ExtraOptions    = {
-	useHash           : false,
-	enableTracing     : false // turn on for console.logging of routing events
-	//preloadingStrategy: PreloadAllModules // uncomment to load all modules lazily
+const config: ExtraOptions = {
+    useHash: false,
+    enableTracing: false // turn on for console.logging of routing events
+    //preloadingStrategy: PreloadAllModules // uncomment to load all modules lazily
 };
 export const AppRoutingModule = RouterModule.forRoot(routes, config);
